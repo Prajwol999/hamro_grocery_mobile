19be13e278a48e75fb0d8ab8e1606aed8c94e2e3
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-06-18 15:02:51 +0545

    feat : Hive constant table

---
+++ lib/app/constant/hive_table_constant.dart
@@ -0,0 +1,13 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  static const int productTableId = 0;
+  static const String productBox = 'productBox';
+
+  static const int categoryTableId = 1;
+  static const String categoryBox = 'categoryBox';
+
+  static const int authTableId = 2;
+
+  static const String authBox = 'userBox';
+}
