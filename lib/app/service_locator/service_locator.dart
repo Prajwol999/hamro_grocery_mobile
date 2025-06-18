ad7059e789ffec620194383dbe70a0dc62edafe5
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-06-18 15:33:45 +0545

    feat : viewmodel authentication

---
+++ lib/app/service_locator/service_locator.dart
@@ -0,0 +1,66 @@
+import 'package:get_it/get_it.dart';
+import 'package:hamro_grocery_mobile/core/network/hive_service.dart';
+import 'package:hamro_grocery_mobile/feature/auth/data/data_source/local_datasource/auth_local_data_source.dart';
+import 'package:hamro_grocery_mobile/feature/auth/data/repository/local_repository/auth_local_repository.dart';
+import 'package:hamro_grocery_mobile/feature/auth/domain/usecase/login_usecase.dart';
+import 'package:hamro_grocery_mobile/feature/auth/domain/usecase/register_usecase.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future initDependencies() async {
+  await _initHiveService();
+  await _initAuthModule();
+  // await _initSplashModule();
+  // await _initHomeModule();
+}
+
+// Future<void> _initSplashModule() async {
+//   serviceLocator.registerFactory(() => SplashViewModel());
+// }
+
+Future _initHiveService() async {
+  serviceLocator.registerLazySingleton<HiveService>(() => HiveService());
+}
+
+Future<void> _initAuthModule() async {
+  // ===================== Data Source ====================
+  serviceLocator.registerFactory(
+    () => UserLocalDataSource(hiveService: serviceLocator<HiveService>()),
+  );
+
+  // ===================== Repository ====================
+
+  serviceLocator.registerFactory(
+    () => AuthLocalRepository(
+      userLocalDataSource: serviceLocator<UserLocalDataSource>(),
+    ),
+  );
+
+  serviceLocator.registerFactory(
+    () =>
+        UserLoginUseCase(authRepository: serviceLocator<AuthLocalRepository>()),
+  );
+
+  serviceLocator.registerFactory(
+    () => UserRegisterUseCase(
+      authRepository: serviceLocator<AuthLocalRepository>(),
+    ),
+  );
+
+  // ===================== ViewModels ====================
+
+  // serviceLocator.registerFactory<RegisterViewModel>(
+  //   () => RegisterViewModel(serviceLocator<UserRegisterUseCase>()),
+  // );
+
+  // Register LoginViewModel WITHOUT HomeViewModel to avoid circular dependency
+//   serviceLocator.registerFactory(
+//     () => LoginViewModel(serviceLocator<UserLoginUseCase>()),
+//   );
+// }
+
+// Future<void> _initHomeModule() async {
+//   serviceLocator.registerFactory(
+//     () => HomeViewModel(loginViewModel: serviceLocator<LoginViewModel>()),
+//   );
+}
---
+++ lib/feature/auth/data/repository/local_repository/auth_local_repository.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:hamro_grocery_mobile/core/error/failure.dart';
+import 'package:hamro_grocery_mobile/feature/auth/data/data_source/local_datasource/auth_local_data_source.dart';
+import 'package:hamro_grocery_mobile/feature/auth/domain/entity/auth_entity.dart';
+import 'package:hamro_grocery_mobile/feature/auth/domain/repository/auth_repository.dart';
+
+class AuthLocalRepository implements IAuthRepository {
+  final UserLocalDataSource _userLocalDataSource;
+
+  AuthLocalRepository({required UserLocalDataSource userLocalDataSource})
+    : _userLocalDataSource = userLocalDataSource;
+
+  @override
+  Future<Either<Failure, String>> loginUser(
+    String email,
+    String password,
+  ) async {
+    try {
+      final result = await _userLocalDataSource.loginUser(email, password);
+      return Right(result);
+    } catch (e) {
+      return Left(LocalDataBaseFailure(message: "Failed to login: $e"));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> registerUser(AuthEntity user) async {
+    try {
+      await _userLocalDataSource.registerUser(user);
+      return Right(null);
+    } catch (e) {
+      return Left(LocalDataBaseFailure(message: "failed to register : $e"));
+    }
+  }
+}
