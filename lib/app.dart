006cccc04c9c1ce01823140355c2b62398658482
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-06-20 22:44:45 +0545

    changes in theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hamro_grocery_mobile/app/service_locator/service_locator.dart';
-import 'package:hamro_grocery_mobile/feature/auth/presentation/view/signin_page.dart';
+import 'package:hamro_grocery_mobile/app/theme/theme.dart';
+
 import 'package:hamro_grocery_mobile/feature/auth/presentation/view_model/login_view_model/login_view_model.dart';
 import 'package:hamro_grocery_mobile/splash/splash_screen.dart';
-
-// import 'package:hamro_grocery_mobile/view/auth/dashboard/grocery_app_home.dart';
-// import 'package:hamro_grocery_mobile/view/auth/dashboard_view.dart';
-import 'common/color_extension.dart';
-// import 'package:hamro_grocery_mobile/splash/splash_screen.dart';
 
 
 class MyApp extends StatelessWidget {
@@ -19,12 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'hamro grocery',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: "Gilroy",
-        colorScheme: ColorScheme.fromSeed(seedColor: TColor.primary),
-        useMaterial3: false,
-      ),
-       home: BlocProvider.value(
+      theme: AppTheme.lightTheme, 
+      home: BlocProvider.value(
         value: serviceLocator<LoginViewModel>(),
         child: SplashScreen(),
       ),
