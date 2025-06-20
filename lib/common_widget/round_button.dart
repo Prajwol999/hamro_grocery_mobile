7df72bba10569c932a918e4f121056d27fe01ee1
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-06-20 22:48:58 +0545

    fixed error

--- lib/common_widget/round_button.dart
+++ lib/common_widget/round_button.dart
@@ -57,7 +57,7 @@ class RoundIconButton extends StatelessWidget {
           Text(
             title,
             style: const TextStyle(
-                color: Colors.white, fontSize: 18, fontWeight: FontWeight.w600),
+                color: Colors.white, fontSize: 18, fontWeight: FontWeight.w800),
           ),
         ],
       ),
