e5c669ae37902261257ad16a3477db2cf28c7b3d
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-07-02 20:14:15 +0545

    feat:some issue solved

--- lib/app/constant/api_endpoints.dart
+++ lib/app/constant/api_endpoints.dart
@@ -3,19 +3,19 @@
 class ApiEndpoints {
   ApiEndpoints._();
 
-  // Timeouts
+  
   static const connectionTimeout = Duration(seconds: 1000);
   static const receiveTimeout = Duration(seconds: 1000);
 
-  // For Android Emulator
+  
   static const String serverAddress = "http://10.0.2.2:8081";
-  // static const String serverAddress = "http://192.168.1.15:8081"; 
+  
 
   static const String baseUrl = "$serverAddress/api/";
   
   static const String imageUrl = "$serverAddress/uploads/";
 
-  //==============Auth ==============
+  
   static const String login = "auth/login";
   static const String register = "auth/register";
 
