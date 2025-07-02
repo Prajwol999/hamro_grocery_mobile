c7a7148babf03a53b280275a8319a996bb769574
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-07-02 20:05:50 +0545

    feat:user api model

--- lib/feature/auth/data/model/user_api_model.dart
+++ lib/feature/auth/data/model/user_api_model.dart
@@ -1,59 +1,59 @@
-// import 'package:equatable/equatable.dart';
-// import 'package:json_annotation/json_annotation.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-// import '../../domain/entity/auth_entity.dart';
+import '../../domain/entity/auth_entity.dart';
 
 
 
-// part 'user_api_model.g.dart';
+part 'user_api_model.g.dart';
 
-// @JsonSerializable()
-// class UserApiModel extends Equatable {
+@JsonSerializable()
+class UserApiModel extends Equatable {
   
-//   @JsonKey(name: '_id', includeIfNull: false)
-//   final String? id;
+  @JsonKey(name: '_id', includeIfNull: false)
+  final String? id;
 
-//   @JsonKey(name: 'fullName')
-//   final String fullName;
+  @JsonKey(name: 'fullName')
+  final String fullName;
 
-//   final String email;
-//   final String? password;
+  final String email;
+  final String? password;
 
-//   const UserApiModel({
-//     this.id,
-//     required this.fullName,
-//     required this.email,
+  const UserApiModel({
+    this.id,
+    required this.fullName,
+    required this.email,
     
-//     this.password,
-//   });
+    this.password,
+  });
 
-//   factory UserApiModel.fromJson(Map<String, dynamic> json) =>
-//       _$UserApiModelFromJson(json);
+  factory UserApiModel.fromJson(Map<String, dynamic> json) =>
+      _$UserApiModelFromJson(json);
 
-//   Map<String, dynamic> toJson() => _$UserApiModelToJson(this);
-
-  
-//   AuthEntity toEntity() {
-//     return AuthEntity(
-//       userId: id,
-//       fullName: fullName,
-//       email: email,
-//       password: password ?? '',
-//     );
-//   }
+  Map<String, dynamic> toJson() => _$UserApiModelToJson(this);
 
   
-//   factory UserApiModel.fromEntity(AuthEntity entity) {
-//     return UserApiModel(
+  AuthEntity toEntity() {
+    return AuthEntity(
+      userId: id,
+      fullName: fullName,
+      email: email,
+      password: password ?? '',
+    );
+  }
+
+  
+  factory UserApiModel.fromEntity(AuthEntity entity) {
+    return UserApiModel(
       
-//       id: entity.userId,
-//       fullName: entity.fullName,
-//       email: entity.email,
-//       password: entity.password,
-//     );
-//   }
+      id: entity.userId,
+      fullName: entity.fullName,
+      email: entity.email,
+      password: entity.password,
+    );
+  }
 
-//   @override
+  @override
   
-//   List<Object?> get props => [id, fullName, password, email];
-// }
+  List<Object?> get props => [id, fullName, password, email];
+}
