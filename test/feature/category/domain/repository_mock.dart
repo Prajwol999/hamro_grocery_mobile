c792e0f5add5e057383fdd8e7e2d4716d7ed52ba
amanch3 <armanachaudhary53@gmail.com>
2025-07-11 21:07:13 +0545

    test : category usecase

---
+++ test/feature/category/domain/repository_mock.dart
@@ -0,0 +1,4 @@
+import 'package:hamro_grocery_mobile/feature/category/domain/repository/category_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockRepository extends Mock implements ICategoryRepository {}
---
+++ test/feature/category/domain/usecase/token_mock.dart
@@ -0,0 +1,4 @@
+import 'package:hamro_grocery_mobile/app/shared_pref/token_shared_pref.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockToken extends Mock implements TokenSharedPrefs {}
