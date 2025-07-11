81d259f9b515137aec00fa2c9fde345672e68daa
Prajwol999 <prajwalneupane385@gmail.com>
2025-07-11 21:17:47 +0545

    test : usecase product passed

---
+++ test/feature/product/domain/mock_repository.dart
@@ -0,0 +1,4 @@
+import 'package:hamro_grocery_mobile/feature/product/domain/repository/product_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockProductRepository extends Mock implements IProductRepository {}
