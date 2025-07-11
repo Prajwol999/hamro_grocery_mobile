287fd6e49bcca8c3151774ff23e092b920d7ee82
amanch3 <armanachaudhary53@gmail.com>
2025-07-11 21:17:47 +0545

    test : usecase product passed

---
+++ test/feature/product/domain/usecase/get_all_product_usecase_test.dart
@@ -0,0 +1,51 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hamro_grocery_mobile/feature/product/domain/entity/product_entity.dart';
+import 'package:hamro_grocery_mobile/feature/product/domain/usecase/get_all_product_usecase.dart';
+import 'package:mocktail/mocktail.dart';
+import '../mock_repository.dart';
+
+void main() {
+  // 2. Declare variables with the CORRECT mock type.
+  late MockProductRepository mockProductRepository;
+  late GetAllProductUsecase getAllProductUsecase;
+
+  setUp(() {
+    // 3. Instantiate the correct mock object.
+    mockProductRepository = MockProductRepository();
+
+    getAllProductUsecase = GetAllProductUsecase(
+      productRepository: mockProductRepository,
+    );
+  });
+
+  test('should get a list of products from the repository', () async {
+    // Arrange
+    final tProductList = [
+      const ProductEntity(
+        productId: '1',
+        name: 'Rice',
+        price: 1000,
+        imageUrl: '',
+      ),
+      const ProductEntity(
+        productId: '2',
+        name: 'Oil',
+        price: 2000,
+        imageUrl: '',
+      ),
+    ];
+
+    when(
+      () => mockProductRepository.getAllProducts(),
+    ).thenAnswer((_) async => Right(tProductList));
+
+    // Act
+    final result = await getAllProductUsecase();
+
+    // Assert
+    expect(result, Right(tProductList));
+    verify(() => mockProductRepository.getAllProducts()).called(1);
+    verifyNoMoreInteractions(mockProductRepository);
+  });
+}
