1daf8e5af9cba397118701e0dddadfc89d1b3333
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-08-03 23:43:38 +0545

    feat:chatbot

--- lib/feature/bot/data/data_source/bot_remote_data_source.dart
+++ lib/feature/bot/data/data_source/bot_remote_data_source.dart
@@ -1,43 +1,43 @@
-// import 'package:dio/dio.dart';
-// import 'package:hamro_grocery_mobile/app/constant/api_endpoints.dart';
-// import 'package:hamro_grocery_mobile/core/network/api_service.dart';
-// import 'package:hamro_grocery_mobile/feature/bot/data/data_source/bot_data_source.dart';
-// import 'package:hamro_grocery_mobile/feature/bot/data/model/bot_model_request.dart';
-// import 'package:hamro_grocery_mobile/feature/bot/data/model/bot_model_response.dart';
-// import 'package:hamro_grocery_mobile/feature/bot/domain/entity/bot_entity.dart';
+import 'package:dio/dio.dart';
+import 'package:hamro_grocery_mobile/app/constant/api_endpoints.dart';
+import 'package:hamro_grocery_mobile/core/network/api_service.dart';
+import 'package:hamro_grocery_mobile/feature/bot/data/data_source/bot_data_source.dart';
+import 'package:hamro_grocery_mobile/feature/bot/data/model/bot_model_request.dart';
+import 'package:hamro_grocery_mobile/feature/bot/data/model/bot_model_response.dart';
+import 'package:hamro_grocery_mobile/feature/bot/domain/entity/bot_entity.dart';
 
-// class BotRemoteDataSource implements BotDataSource {
-//   final ApiService _apiService;
+class BotRemoteDataSource implements BotDataSource {
+  final ApiService _apiService;
 
-//   BotRemoteDataSource({required ApiService apiService})
-//     : _apiService = apiService;
+  BotRemoteDataSource({required ApiService apiService})
+    : _apiService = apiService;
 
-//   @override
-//   Future<ChatResponseModel> getChatReply({
-//     required String query,
-//     required List<ChatMessageEntity> history,
-//   }) async {
-//     try {
-//       final requestModel = ChatRequestModel.fromEntities(
-//         query: query,
-//         historyEntities: history,
-//       );
-//       final response = await _apiService.dio.post(
-//         ApiEndpoints.chatQuery,
-//         data: requestModel.toJson(),
-//       );
+  @override
+  Future<ChatResponseModel> getChatReply({
+    required String query,
+    required List<ChatMessageEntity> history,
+  }) async {
+    try {
+      final requestModel = ChatRequestModel.fromEntities(
+        query: query,
+        historyEntities: history,
+      );
+      final response = await _apiService.dio.post(
+        ApiEndpoints.chatQuery,
+        data: requestModel.toJson(),
+      );
 
-//       if (response.statusCode == 200) {
-//         return ChatResponseModel.fromJson(response.data);
-//       } else {
-//         throw Exception("Chatbot failed to respond: ${response.statusMessage}");
-//       }
-//     } on DioException catch (e) {
-//       throw Exception(
-//         "Chatbot connection error: ${e.response?.data['message'] ?? e.message}",
-//       );
-//     } catch (e) {
-//       throw Exception("An unexpected error occurred: $e");
-//     }
-//   }
-// }
+      if (response.statusCode == 200) {
+        return ChatResponseModel.fromJson(response.data);
+      } else {
+        throw Exception("Chatbot failed to respond: ${response.statusMessage}");
+      }
+    } on DioException catch (e) {
+      throw Exception(
+        "Chatbot connection error: ${e.response?.data['message'] ?? e.message}",
+      );
+    } catch (e) {
+      throw Exception("An unexpected error occurred: $e");
+    }
+  }
+}
