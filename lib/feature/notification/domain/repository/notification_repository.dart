bbb633f23882f90ea387bf6718bc5b9977f4b482
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-08-03 23:35:28 +0545

    feat:notification

--- lib/feature/notification/domain/repository/notification_repository.dart
+++ lib/feature/notification/domain/repository/notification_repository.dart
@@ -1,9 +1,9 @@
-// import 'package:dartz/dartz.dart';
-// import 'package:hamro_grocery_mobile/feature/notification/domain/entity/notification_entity.dart';
+import 'package:dartz/dartz.dart';
+import 'package:hamro_grocery_mobile/feature/notification/domain/entity/notification_entity.dart';
 
-// import '../../../../core/error/failure.dart';
+import '../../../../core/error/failure.dart';
 
-// abstract class INotificationRepository {
-//   Future<Either<Failure, List<NotificationEntity>>> getNotifications();
-//   Future<Either<Failure, bool>> markAsRead();
-// }
+abstract class INotificationRepository {
+  Future<Either<Failure, List<NotificationEntity>>> getNotifications();
+  Future<Either<Failure, bool>> markAsRead();
+}
