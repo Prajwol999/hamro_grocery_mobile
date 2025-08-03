9b92a52021f17e4a7789c6f82e463248876d1ed7
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-08-03 23:35:48 +0545

    feat:notification

--- lib/feature/notification/domain/use_case/get_notifications_usecase.dart
+++ lib/feature/notification/domain/use_case/get_notifications_usecase.dart
@@ -1,19 +1,19 @@
-// import 'package:dartz/dartz.dart';
-// import 'package:hamro_grocery_mobile/app/usecase/usecase.dart';
-// import 'package:hamro_grocery_mobile/core/error/failure.dart';
-// import 'package:hamro_grocery_mobile/feature/notification/domain/entity/notification_entity.dart';
-// import 'package:hamro_grocery_mobile/feature/notification/domain/repository/notification_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:hamro_grocery_mobile/app/usecase/usecase.dart';
+import 'package:hamro_grocery_mobile/core/error/failure.dart';
+import 'package:hamro_grocery_mobile/feature/notification/domain/entity/notification_entity.dart';
+import 'package:hamro_grocery_mobile/feature/notification/domain/repository/notification_repository.dart';
 
-// // FINAL FIX: Implement UseCaseWithoutParams
-// class GetNotificationsUseCase
-//     implements UseCaseWithoutParams<List<NotificationEntity>> {
-//   final INotificationRepository _notificationRepository;
+// FINAL FIX: Implement UseCaseWithoutParams
+class GetNotificationsUseCase
+    implements UseCaseWithoutParams<List<NotificationEntity>> {
+  final INotificationRepository _notificationRepository;
 
-//   GetNotificationsUseCase(this._notificationRepository);
+  GetNotificationsUseCase(this._notificationRepository);
 
-//   @override
-//   // FINAL FIX: The call method takes no parameters
-//   Future<Either<Failure, List<NotificationEntity>>> call() async {
-//     return await _notificationRepository.getNotifications();
-//   }
-// }
+  @override
+  // FINAL FIX: The call method takes no parameters
+  Future<Either<Failure, List<NotificationEntity>>> call() async {
+    return await _notificationRepository.getNotifications();
+  }
+}
