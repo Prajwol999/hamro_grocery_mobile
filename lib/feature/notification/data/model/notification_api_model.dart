382046745222613dbc27e752f9ef764e82d298ff
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-08-03 23:33:52 +0545

    feat:notification

--- lib/feature/notification/data/model/notification_api_model.dart
+++ lib/feature/notification/data/model/notification_api_model.dart
@@ -1,26 +1,26 @@
-// import 'package:json_annotation/json_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-// part 'notification_api_model.g.dart';
+part 'notification_api_model.g.dart';
 
-// @JsonSerializable()
-// class NotificationApiModel {
-//   @JsonKey(name: '_id')
-//   final String id;
-//   final String userId;
-//   final String message;
-//   final bool read;
-//   final DateTime createdAt;
+@JsonSerializable()
+class NotificationApiModel {
+  @JsonKey(name: '_id')
+  final String id;
+  final String userId;
+  final String message;
+  final bool read;
+  final DateTime createdAt;
 
-//   NotificationApiModel({
-//     required this.id,
-//     required this.userId,
-//     required this.message,
-//     required this.read,
-//     required this.createdAt,
-//   });
+  NotificationApiModel({
+    required this.id,
+    required this.userId,
+    required this.message,
+    required this.read,
+    required this.createdAt,
+  });
 
-//   factory NotificationApiModel.fromJson(Map<String, dynamic> json) =>
-//       _$NotificationApiModelFromJson(json);
+  factory NotificationApiModel.fromJson(Map<String, dynamic> json) =>
+      _$NotificationApiModelFromJson(json);
 
-//   Map<String, dynamic> toJson() => _$NotificationApiModelToJson(this);
-// }
+  Map<String, dynamic> toJson() => _$NotificationApiModelToJson(this);
+}
