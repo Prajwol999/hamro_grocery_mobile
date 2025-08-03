1bec3550c755986b04d12247af889280ce0c66a7
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-08-03 23:34:34 +0545

    feat:notification

--- lib/feature/notification/data/model/notification_api_model.g.dart
+++ lib/feature/notification/data/model/notification_api_model.g.dart
@@ -1,27 +1,27 @@
-// // GENERATED CODE - DO NOT MODIFY BY HAND
+// GENERATED CODE - DO NOT MODIFY BY HAND
 
-// part of 'notification_api_model.dart';
+part of 'notification_api_model.dart';
 
-// // **************************************************************************
-// // JsonSerializableGenerator
-// // **************************************************************************
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
 
-// NotificationApiModel _$NotificationApiModelFromJson(
-//         Map<String, dynamic> json) =>
-//     NotificationApiModel(
-//       id: json['_id'] as String,
-//       userId: json['userId'] as String,
-//       message: json['message'] as String,
-//       read: json['read'] as bool,
-//       createdAt: DateTime.parse(json['createdAt'] as String),
-//     );
+NotificationApiModel _$NotificationApiModelFromJson(
+        Map<String, dynamic> json) =>
+    NotificationApiModel(
+      id: json['_id'] as String,
+      userId: json['userId'] as String,
+      message: json['message'] as String,
+      read: json['read'] as bool,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
 
-// Map<String, dynamic> _$NotificationApiModelToJson(
-//         NotificationApiModel instance) =>
-//     <String, dynamic>{
-//       '_id': instance.id,
-//       'userId': instance.userId,
-//       'message': instance.message,
-//       'read': instance.read,
-//       'createdAt': instance.createdAt.toIso8601String(),
-//     };
+Map<String, dynamic> _$NotificationApiModelToJson(
+        NotificationApiModel instance) =>
+    <String, dynamic>{
+      '_id': instance.id,
+      'userId': instance.userId,
+      'message': instance.message,
+      'read': instance.read,
+      'createdAt': instance.createdAt.toIso8601String(),
+    };
