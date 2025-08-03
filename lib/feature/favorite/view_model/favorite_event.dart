d21e3cbecc7585eeb205bbc3be6ebee4ad1b7706
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-08-03 23:30:31 +0545

    feat:favourite

--- lib/feature/favorite/view_model/favorite_event.dart
+++ lib/feature/favorite/view_model/favorite_event.dart
@@ -1,15 +1,15 @@
-// import 'package:flutter/material.dart';
-// import 'package:hamro_grocery_mobile/feature/product/domain/entity/product_entity.dart';
+import 'package:flutter/material.dart';
+import 'package:hamro_grocery_mobile/feature/product/domain/entity/product_entity.dart';
 
-// @immutable
-// abstract class FavoriteEvent {}
+@immutable
+abstract class FavoriteEvent {}
 
-// class AddToFavorites extends FavoriteEvent {
-//   final ProductEntity product;
-//   AddToFavorites(this.product);
-// }
+class AddToFavorites extends FavoriteEvent {
+  final ProductEntity product;
+  AddToFavorites(this.product);
+}
 
-// class RemoveFromFavorites extends FavoriteEvent {
-//   final String productId;
-//   RemoveFromFavorites(this.productId);
-// }
+class RemoveFromFavorites extends FavoriteEvent {
+  final String productId;
+  RemoveFromFavorites(this.productId);
+}
