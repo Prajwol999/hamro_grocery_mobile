2571a2af458f11d47514d8d8128c9202c271ea0e
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-08-03 23:45:16 +0545

    feat:chatbot

--- lib/feature/bot/data/model/bot_model_response.dart
+++ lib/feature/bot/data/model/bot_model_response.dart
@@ -1,50 +1,50 @@
-// import 'package:json_annotation/json_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-// part 'bot_model_response.g.dart';
+part 'bot_model_response.g.dart';
 
-// ChatReplyDataModel _dataFromJson(Map<String, dynamic>? json) {
-//   if (json == null) {
-//     return const ChatReplyDataModel(
-//       reply: 'Sorry, I could not get a response.',
-//     );
-//   }
-//   return ChatReplyDataModel.fromJson(json);
-// }
+ChatReplyDataModel _dataFromJson(Map<String, dynamic>? json) {
+  if (json == null) {
+    return const ChatReplyDataModel(
+      reply: 'Sorry, I could not get a response.',
+    );
+  }
+  return ChatReplyDataModel.fromJson(json);
+}
 
-// @JsonSerializable(explicitToJson: true)
-// class ChatResponseModel {
-//   @JsonKey(defaultValue: 500)
-//   final int statusCode;
+@JsonSerializable(explicitToJson: true)
+class ChatResponseModel {
+  @JsonKey(defaultValue: 500)
+  final int statusCode;
 
-//   // Use the custom fromJson helper function here
-//   @JsonKey(name: 'data', fromJson: _dataFromJson)
-//   final ChatReplyDataModel data;
+  // Use the custom fromJson helper function here
+  @JsonKey(name: 'data', fromJson: _dataFromJson)
+  final ChatReplyDataModel data;
 
-//   @JsonKey(defaultValue: 'An unknown error occurred.')
-//   final String message;
+  @JsonKey(defaultValue: 'An unknown error occurred.')
+  final String message;
 
-//   ChatResponseModel({
-//     required this.statusCode,
-//     required this.data,
-//     required this.message,
-//   });
+  ChatResponseModel({
+    required this.statusCode,
+    required this.data,
+    required this.message,
+  });
 
-//   factory ChatResponseModel.fromJson(Map<String, dynamic> json) =>
-//       _$ChatResponseModelFromJson(json);
+  factory ChatResponseModel.fromJson(Map<String, dynamic> json) =>
+      _$ChatResponseModelFromJson(json);
 
-//   Map<String, dynamic> toJson() => _$ChatResponseModelToJson(this);
-// }
+  Map<String, dynamic> toJson() => _$ChatResponseModelToJson(this);
+}
 
-// @JsonSerializable()
-// class ChatReplyDataModel {
-//   @JsonKey(defaultValue: 'Sorry, no reply was found.')
-//   final String reply;
+@JsonSerializable()
+class ChatReplyDataModel {
+  @JsonKey(defaultValue: 'Sorry, no reply was found.')
+  final String reply;
 
-//   // It's good practice to keep the const constructor
-//   const ChatReplyDataModel({required this.reply});
+  // It's good practice to keep the const constructor
+  const ChatReplyDataModel({required this.reply});
 
-//   factory ChatReplyDataModel.fromJson(Map<String, dynamic> json) =>
-//       _$ChatReplyDataModelFromJson(json);
+  factory ChatReplyDataModel.fromJson(Map<String, dynamic> json) =>
+      _$ChatReplyDataModelFromJson(json);
 
-//   Map<String, dynamic> toJson() => _$ChatReplyDataModelToJson(this);
-// }
+  Map<String, dynamic> toJson() => _$ChatReplyDataModelToJson(this);
+}
--- lib/feature/bot/data/model/bot_model_response.g.dart
+++ lib/feature/bot/data/model/bot_model_response.g.dart
@@ -1,31 +1,31 @@
-// // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// part of 'bot_model_response.dart';
 
-// // **************************************************************************
-// // JsonSerializableGenerator
-// // **************************************************************************
+part of 'bot_model_response.dart';
 
-// ChatResponseModel _$ChatResponseModelFromJson(Map<String, dynamic> json) =>
-//     ChatResponseModel(
-//       statusCode: (json['statusCode'] as num?)?.toInt() ?? 500,
-//       data: _dataFromJson(json['data'] as Map<String, dynamic>?),
-//       message: json['message'] as String? ?? 'An unknown error occurred.',
-//     );
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
 
-// Map<String, dynamic> _$ChatResponseModelToJson(ChatResponseModel instance) =>
-//     <String, dynamic>{
-//       'statusCode': instance.statusCode,
-//       'data': instance.data.toJson(),
-//       'message': instance.message,
-//     };
+ChatResponseModel _$ChatResponseModelFromJson(Map<String, dynamic> json) =>
+    ChatResponseModel(
+      statusCode: (json['statusCode'] as num?)?.toInt() ?? 500,
+      data: _dataFromJson(json['data'] as Map<String, dynamic>?),
+      message: json['message'] as String? ?? 'An unknown error occurred.',
+    );
 
-// ChatReplyDataModel _$ChatReplyDataModelFromJson(Map<String, dynamic> json) =>
-//     ChatReplyDataModel(
-//       reply: json['reply'] as String? ?? 'Sorry, no reply was found.',
-//     );
+Map<String, dynamic> _$ChatResponseModelToJson(ChatResponseModel instance) =>
+    <String, dynamic>{
+      'statusCode': instance.statusCode,
+      'data': instance.data.toJson(),
+      'message': instance.message,
+    };
 
-// Map<String, dynamic> _$ChatReplyDataModelToJson(ChatReplyDataModel instance) =>
-//     <String, dynamic>{
-//       'reply': instance.reply,
-//     };
+ChatReplyDataModel _$ChatReplyDataModelFromJson(Map<String, dynamic> json) =>
+    ChatReplyDataModel(
+      reply: json['reply'] as String? ?? 'Sorry, no reply was found.',
+    );
+
+Map<String, dynamic> _$ChatReplyDataModelToJson(ChatReplyDataModel instance) =>
+    <String, dynamic>{
+      'reply': instance.reply,
+    };
