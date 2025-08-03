d58f6301fa228ded15e9c68d9ac59e115b4ca585
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-08-03 23:37:16 +0545

    feat:notification

--- lib/feature/notification/presentation/view_model/notification_state.dart
+++ lib/feature/notification/presentation/view_model/notification_state.dart
@@ -1,33 +1,33 @@
-// import 'package:equatable/equatable.dart';
-// import 'package:hamro_grocery_mobile/feature/notification/domain/entity/notification_entity.dart';
+import 'package:equatable/equatable.dart';
+import 'package:hamro_grocery_mobile/feature/notification/domain/entity/notification_entity.dart';
 
-// class NotificationState extends Equatable {
-//   final bool isLoading;
-//   final String? error;
-//   final List<NotificationEntity> notifications;
-//   final int unreadCount;
+class NotificationState extends Equatable {
+  final bool isLoading;
+  final String? error;
+  final List<NotificationEntity> notifications;
+  final int unreadCount;
 
-//   const NotificationState({
-//     this.isLoading = false,
-//     this.error,
-//     this.notifications = const [],
-//     this.unreadCount = 0,
-//   });
+  const NotificationState({
+    this.isLoading = false,
+    this.error,
+    this.notifications = const [],
+    this.unreadCount = 0,
+  });
 
-//   NotificationState copyWith({
-//     bool? isLoading,
-//     String? error,
-//     List<NotificationEntity>? notifications,
-//     int? unreadCount,
-//   }) {
-//     return NotificationState(
-//       isLoading: isLoading ?? this.isLoading,
-//       error: error ?? this.error,
-//       notifications: notifications ?? this.notifications,
-//       unreadCount: unreadCount ?? this.unreadCount,
-//     );
-//   }
+  NotificationState copyWith({
+    bool? isLoading,
+    String? error,
+    List<NotificationEntity>? notifications,
+    int? unreadCount,
+  }) {
+    return NotificationState(
+      isLoading: isLoading ?? this.isLoading,
+      error: error ?? this.error,
+      notifications: notifications ?? this.notifications,
+      unreadCount: unreadCount ?? this.unreadCount,
+    );
+  }
 
-//   @override
-//   List<Object?> get props => [isLoading, error, notifications, unreadCount];
-// }
+  @override
+  List<Object?> get props => [isLoading, error, notifications, unreadCount];
+}
